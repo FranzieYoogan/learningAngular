724da6e7a3cdc34fe930e06653f6480f3577e2cb
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-02-04 11:02:31 -0300

    someDesign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientJsonpModule } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
     AboutComponent,
     HomeComponent,
 
-    HeaderComponent
+    HeaderComponent,
+     FooterComponent
   ],
   imports: [
     BrowserModule,

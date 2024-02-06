6b05791911b52f7aa1ea30de5ce4ef92347c17e4
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-02-06 14:17:47 -0300

    someMistakesFixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,11 +35,15 @@ export class HomeComponent {
     const section: any = document.getElementById('contaner2Style')
     const search: any = document.getElementById('inputStyle')
     const inputStyle:any = document.getElementById('inputStyle')
+    const inputStyle2:any = document.getElementById('inputStyle2')
+
     section.style.opacity = '1'
 
     inputStyle.style.cursor = 'text'
     inputStyle.style.border = '1px solid black'
+    inputStyle2.style.border = '1px solid black'
     inputStyle.disabled = false
+    inputStyle2.disabled = false
     
     buttonClose.style.visibility ='hidden'
 
@@ -50,6 +54,7 @@ export class HomeComponent {
 
   closeMenu2() {
     const inputStyle:any = document.getElementById('inputStyle')
+    const inputStyle2:any = document.getElementById('inputStyle2')
     const categoriesButton: any = document.getElementById('dropdownDefaultButton')
     const buttonBubbleChat2: any = document.getElementById('bubbleChatContainer2')
     const section: any = document.getElementById('contaner2Style')
@@ -58,6 +63,8 @@ export class HomeComponent {
     inputStyle.style.cursor = 'text'
     inputStyle.style.border = '1px solid black'
     inputStyle.disabled = false
+    inputStyle2.style.border = '1px solid black'
+    inputStyle2.disabled = false
 
 
     categoriesButton.style.borderBottom = '1px solid transparent'
@@ -107,6 +114,40 @@ export class HomeComponent {
     const spanResultCategories2:any = document.getElementById('spanResultCategories2')
     const spanResultCategories3:any = document.getElementById('spanResultCategories3')
     const spanResultCategories4:any = document.getElementById('spanResultCategories4')
+    const buttonBubbleChat2: any = document.getElementById('dropdownDefaultButton')
+    const bubbleChat:any =  document.getElementById('bubbleChatContainer')
+    var currentTime = new Date().toLocaleString()
+    const bubbleChat2:any =  document.getElementById('bubbleChatContainer2')
+    const span2:any = document.getElementById('spanTime2')
+    const inputStyle:any = document.getElementById('inputStyle')
+    const inputStyle2:any = document.getElementById('inputStyle2')
+    const categoriesResult:any = document.getElementById('categoriesResult')
+
+  console.log(currentTime)
+   bubbleChat2.style.visibility = 'visible'
+
+   const section: any = document.getElementById('contaner2Style')
+
+
+   inputStyle.style.cursor = 'not-allowed'
+   inputStyle.style.border = '2px solid red'
+   inputStyle.disabled = true
+
+   inputStyle2.style.border = '2px solid red' 
+   inputStyle2.style.cursor = 'not-allowed'
+   inputStyle2.disabled = true
+
+
+
+   buttonBubbleChat2.style.borderBottom = '1px solid orange'
+   buttonBubbleChat2.style.borderRadius = '0px'
+   buttonBubbleChat2.style.width = '100%'
+   buttonBubbleChat2.style.transition = '1s'
+   bubbleChat.style.visibility = 'hidden'
+   section.style.opacity = '0.3'
+
+
+
 
       this.http.get(`https://api.chucknorris.io/jokes/categories`).subscribe((response) => {
 
@@ -133,33 +174,8 @@ export class HomeComponent {
  
 
 
-    const search: any = document.getElementById('inputStyle')
-    const buttonBubbleChat2: any = document.getElementById('dropdownDefaultButton')
-    const bubbleChat:any =  document.getElementById('bubbleChatContainer')
-    var currentTime = new Date().toLocaleString()
-    const bubbleChat2:any =  document.getElementById('bubbleChatContainer2')
-    const span2:any = document.getElementById('spanTime2')
-    const inputStyle:any = document.getElementById('inputStyle')
-    const categoriesResult:any = document.getElementById('categoriesResult')
 
-  console.log(currentTime)
-   bubbleChat2.style.visibility = 'visible'
-
-   const section: any = document.getElementById('contaner2Style')
-
-
-   inputStyle.style.cursor = 'not-allowed'
-   inputStyle.disabled = true
-
-
-
-   buttonBubbleChat2.style.borderBottom = '1px solid orange'
-   search.style.border = '1px solid black'
-   buttonBubbleChat2.style.borderRadius = '0px'
-   buttonBubbleChat2.style.width = '100%'
-   buttonBubbleChat2.style.transition = '1s'
-   bubbleChat.style.visibility = 'hidden'
-   section.style.opacity = '0.3'
+ 
    return    span2.innerHTML = currentTime.toString()
 
 
@@ -211,8 +227,12 @@ ngOnInit(): void {
         searchValue.style.transition = '0s'
        buttonSearch.click()
     }  else {
+      searchValue.style.cursor ='not-allowed'
+      searchValue.disabled = true
+      searchValue.style.transition = '0s'
       bubbleChat.style.visibility ='visible'
       searchValue.style.border = '2px solid red'
+  
 
 
 
@@ -222,19 +242,27 @@ ngOnInit(): void {
 
   searchValue2.addEventListener('keypress', function (e: { key: string; }) {
   if (e.key === 'Enter') {
+    if(searchValue2.value) {
     const buttonSearch2:any = document.getElementById('buttonSearch2')
-     searchValue2.style.cursor ='not-allowed'
-     searchValue2.disabled = true
+    
+    searchValue2.style.cursor ='not-allowed'
+        searchValue2.disabled = true
+        searchValue2.style.border = '2px solid green'
+        searchValue2.style.transition = '0s'
 
 buttonSearch2.click()
-
-  } else {
+    
+}
+  else {
+    searchValue2.style.cursor ='not-allowed'
+    searchValue2.disabled = true
+    searchValue2.style.transition = '0s'
     bubbleChat.style.visibility ='visible'
     searchValue2.style.border = '2px solid red'
 
 
   }
-
+} 
     });
 
 
